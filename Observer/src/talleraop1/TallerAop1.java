e522b7014bd0e108570f99387135ce71a1f8b207
AToala <aitoala@espol.edu.ec>
2020-11-07 19:10:33 -0500

    Agregando botones y sus acciones

--- Observer/src/talleraop1/TallerAop1.java
+++ Observer/src/talleraop1/TallerAop1.java
@@ -1,38 +1,58 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package talleraop1;
+
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
-
 /**
  *
- * @author dylan
+ * @author TeamOne
  */
 public class TallerAop1 extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
+        HBox root = new HBox();
+        root.setAlignment(Pos.CENTER);
+        Button btnRed = new Button();
+        btnRed.setText("Red Card");
+        Button btnBlue = new Button();
+        btnBlue.setText("Blue Card");
+        Button btnGreen = new Button();
+        btnGreen.setText("Green Card");
+
+        btnRed.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+            	root.setStyle("-fx-background-color:RED;");
+            	System.out.println("RED...");
+            }
+        });
+        btnBlue.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+            	root.setStyle("-fx-background-color:BLUE;");
+            	System.out.println("BLUE...");
+            }
+        });
+        btnGreen.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+            	root.setStyle("-fx-background-color:GREEN;");
+            	System.out.println("GREEN...");
             }
         });
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        
+        root.getChildren().add(btnRed);
+        root.getChildren().add(btnBlue);
+        root.getChildren().add(btnGreen);
+        
         
         Scene scene = new Scene(root, 300, 250);
         
